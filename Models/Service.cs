a53bec6d25f78e75ffdec42eb370dd4306c54001
Austin Perrine <akperrine@gmail.com>
2024-09-26 08:38:39 -1000

    add Service model

---
+++ Models/Service.cs
@@ -0,0 +1,10 @@
+
+namespace MobilePractice.Models;
+
+public class Service {
+    public long Id {get; set;}
+    public long PractitionerId {get; set;}
+    public string? Name {get; set;}
+    public int Duration {get; set;}
+
+}
