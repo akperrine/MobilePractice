e9c7b5bf4c4765a29597314d202c1f2c385f4898
Austin Perrine <akperrine@gmail.com>
2024-10-04 21:25:26 -0700

    create Treatment CRUD controller and service

---
+++ Controllers/TreatmentController.cs
@@ -0,0 +1,57 @@
+
+using Microsoft.AspNetCore.Mvc;
+using MobilePractice.Models;
+using MobilePractice.Services;
+
+namespace MobilePractice.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TreatmentController : ControllerBase {
+    
+    TreatmentService treatmentService;
+    public TreatmentController(TreatmentService service) {
+        treatmentService = service;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Treatment>>> GetAllTreatments() {
+         return await treatmentService.GetAll();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Treatment>> GetTreatmentById(long id) {
+        var result = await treatmentService.GetTreatmentById(id);
+        if (result == null) {
+            return BadRequest();
+        }
+        return Ok(result);
+    }
+
+    [HttpPost("/create")]
+    public async Task<ActionResult<Treatment>> CreateTreatment([FromBody] Treatment treatment) {
+        var result = await treatmentService.CreateTreatment(treatment);
+        if (result == null) {
+            return BadRequest();
+        }
+        return Ok(result);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult<Treatment>> UpdateTreatment([FromBody] Treatment treatment) {
+        var result = await treatmentService.UpdateTreatment(treatment);
+        if (result == null) {
+            return BadRequest();
+        }
+        return Ok(result);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<Treatment>> DeleteTreatment(long id) {
+        var result = await treatmentService.DeleteTreatment(id);
+        if (result == null) {
+            return BadRequest();
+        }
+        return Ok(result);
+    }
+}
---
+++ Services/TreatmentService.cs
@@ -0,0 +1,56 @@
+using MobilePractice.Data;
+using MobilePractice.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MobilePractice.Services;
+public class TreatmentService {
+    private readonly PracticeContext _context;
+
+    public TreatmentService(PracticeContext context) {
+        _context = context;
+    }
+
+    public async Task<List<Treatment>> GetAll() => await _context.Treatments.ToListAsync();
+
+    public async Task<Treatment?> GetTreatmentById(long id) {
+        var treatment = await _context.Treatments
+                .FirstOrDefaultAsync(t => t.Id == id);
+
+            if (treatment == null)
+            {
+                return null;
+            }
+            return treatment;
+    }
+
+    public async Task<Treatment?> CreateTreatment(Treatment treatment) {
+        var existingTreatment = await _context.Treatments.Where(t => t.Id == treatment.Id).FirstOrDefaultAsync();
+
+        if (existingTreatment != null) {
+            return null;
+        };
+        DateTime timestamp = DateTime.UtcNow;
+        Treatment newTreatment = new Treatment {
+            // TODO()
+        };
+        await _context.AddAsync(newTreatment);
+        await _context.SaveChangesAsync();
+
+        return treatment;
+    }
+
+    public async Task<Treatment?> UpdateTreatment(Treatment treatment) {
+        _context.Treatments.Update(treatment);
+        await _context.SaveChangesAsync();
+
+        return treatment; 
+    }
+
+    public async Task<Treatment?> DeleteTreatment(long id) {
+        var treatment = await _context.Treatments.SingleAsync(t => t.Id == id);
+        _context.Treatments.Remove(treatment);
+        await _context.SaveChangesAsync();
+
+        return treatment;
+    }
+}
